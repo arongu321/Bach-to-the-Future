caa88860f52018022d0e321b0d9fb053f9a47379
Bach <waridh@ualberta.ca>
2022-01-15 14:13:14 -0700

    learned how to use csv

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import csv
 import os
 
-def the_output_csv(filename):
+def the_output_csv(filename, theData):
     """
     The function that will output the data collected as a .csv file type.
     """
-    address = os.path().join('Outputs', os.sep(), "\%s.csv"%(filename))
-    print(address)
+    address = "Outputs\\%s.csv"%(filename);
+    header = []
+    with open(address, 'w') as fout:
+        writer = csv.writer(fout);
+
+        writer.writerow(theData);
 
 if __name__ == "__main__":
-
+    databoi = ["test", 69, "cool"]
+    the_output_csv("test", databoi);
