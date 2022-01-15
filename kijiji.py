207e93fbd09a6c75cfebbdfe7034f06b5180756f
Nikolai <notnikolai1@gmail.com>
2022-01-15 15:14:24 -0700

    Scrape search results

--- kijiji.py
+++ kijiji.py
@@ -16,8 +16,31 @@ def get_kijiji_page_info(url):
     print(body)
     return(price, title, content)
 
+def make_kijiji_search_url(search_string, region = 'edmonton', ):
+    search_string = search_string.lower().replace(' ', '-')
 
-url = 'https://www.kijiji.ca/v-monitors/edmonton/monitor-arm-dual-monitor-arm/1601268382?cid=e507e4e1-63d8-4af4-b337-63a18f4c6c07'
+    region_dict = {'edmonton' : 'k0c10l1700203',
+                   'calgary' : 'k0c10l1700199'}
 
-for i in get_kijiji_page_info(url):
-    print(i)
+    url = "https://www.kijiji.ca/b-buy-sell/" + region + "/" + search_string + "/" + region_dict[region] + "?sort=dateAsc&dc=true"
+
+    return url
+
+def get_kijiji_search_results(url):
+    print(url)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    html = list(soup.children)[2]
+    head = list(html.children)[1]
+    body = list(html.children)[3]
+
+    return ("https://www.kijiji.ca/" + i.find('a')['href'] for i in list(body.find_all('div', class_='title')))
+
+
+[print(i) for i in get_kijiji_search_results(make_kijiji_search_url('AMD RYZEN', region='edmonton'))]
+
+#url = 'https://www.kijiji.ca/v-monitors/edmonton/monitor-arm-dual-monitor-arm/1601268382?cid=e507e4e1-63d8-4af4-b337-63a18f4c6c07'
+
+#for i in get_kijiji_page_info(url):
+#    print(i)
