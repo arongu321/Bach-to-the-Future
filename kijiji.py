91e40a350695787c6a7209ab3e00135a95ba6771
Nikolai <notnikolai1@gmail.com>
2022-01-15 14:38:20 -0700

    get_kijiji_page_info() more or less done

--- kijiji.py
+++ kijiji.py
@@ -3,21 +3,21 @@ from bs4 import BeautifulSoup
 
 def get_kijiji_page_info(url):
     page = requests.get(url)
-
-    #print(page.content)
     soup = BeautifulSoup(page.content, 'html.parser')
-    #print(soup.prettify())
 
     html = list(soup.children)[2]
-
     head = list(html.children)[1]
-
     body = list(html.children)[3]
 
     price = list(body.find_all('div', class_='priceContainer-1419890179'))[0].find('span').find('span').get_text()
-    return(price)
+    title = head.find('title').get_text()
+    content = list(body.find_all('div', class_='descriptionContainer-231909819'))[0].find('div').get_text()
+
+    print(body)
+    return(price, title, content)
 
 
 url = 'https://www.kijiji.ca/v-monitors/edmonton/monitor-arm-dual-monitor-arm/1601268382?cid=e507e4e1-63d8-4af4-b337-63a18f4c6c07'
 
-print(get_kijiji_page_info(url))
+for i in get_kijiji_page_info(url):
+    print(i)
