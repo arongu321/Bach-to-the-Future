94982a06c9053e1b15df9cd159fbb23d573f5e3b
Nikolai <notnikolai1@gmail.com>
2022-01-15 16:02:24 -0700

    Ready to integrate

--- kijiji.py
+++ kijiji.py
@@ -1,20 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+
+def get_page(url):
+    try:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        if len(list(soup.children)) < 3:
+            print('invalid, wait 5s')
+            time.sleep(5)
+            page = requests.get(url)  # try again if the response was incorrect
+            soup = BeautifulSoup(page.content, 'html.parser')
+
+            if len(list(soup.children)) < 3:
+                print('still invalid, give up')
+                return None
+        return soup
+    except:
+        return None
 
 def get_kijiji_page_info(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    category = url[22:].split('/')[0]
+    soup = get_page(url)
+
+    if not soup:
+        return None
 
     html = list(soup.children)[2]
     head = list(html.children)[1]
     body = list(html.children)[3]
 
     price = list(body.find_all('div', class_='priceContainer-1419890179'))[0].find('span').find('span').get_text()
+
+    currency = list(body.find_all('div', class_='priceContainer-1419890179'))[0].find('span').find_all('span')[1]['content']
+    print(currency)
     title = head.find('title').get_text()
     content = list(body.find_all('div', class_='descriptionContainer-231909819'))[0].find('div').get_text()
 
-    print(body)
-    return(price, title, content)
+    return{'price': [price, currency], 'title': title, 'category': category, 'content': content}
+
 
 def make_kijiji_search_url(search_string, region = 'edmonton', ):
     search_string = search_string.lower().replace(' ', '-')
@@ -27,7 +52,6 @@ def make_kijiji_search_url(search_string, region = 'edmonton', ):
     return url
 
 def get_kijiji_search_results(url):
-    print(url)
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -35,12 +59,11 @@ def get_kijiji_search_results(url):
     head = list(html.children)[1]
     body = list(html.children)[3]
 
-    return ("https://www.kijiji.ca/" + i.find('a')['href'] for i in list(body.find_all('div', class_='title')))
+    return ["https://www.kijiji.ca" + i.find('a')['href'] for i in list(body.find_all('div', class_='title'))]
 
+if __name__ == "__main__":
+    url_list = get_kijiji_search_results(make_kijiji_search_url('amd'))
 
-[print(i) for i in get_kijiji_search_results(make_kijiji_search_url('AMD RYZEN', region='edmonton'))]
-
-#url = 'https://www.kijiji.ca/v-monitors/edmonton/monitor-arm-dual-monitor-arm/1601268382?cid=e507e4e1-63d8-4af4-b337-63a18f4c6c07'
-
-#for i in get_kijiji_page_info(url):
-#    print(i)
+    #for url in url_list:
+    #    print(url)
+    #    print(get_kijiji_page_info(url))
