40fe2e4f55b1975b0a41d4dd72173c3592021085
Bach <waridh@ualberta.ca>
2022-01-15 15:17:37 -0700

    category added to storageBoi

--- storageBoi.py
+++ storageBoi.py
@@ -1,10 +1,11 @@
 class StorageBoi:
     def __init__(self, pricE = [None, None], urL = '', titlE = '',
-    descriptioN = ''):
+    descriptioN = '', categorY = ''):
         self.price = pricE;
         self.url = urL;
         self.title = titlE;
         self.description = descriptioN;
+        self.category = categorY;
     
     def outlist(self):
         return [self.price, self.title, self.description, self.url];
