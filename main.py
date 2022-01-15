716ffd249e97f0c3b6016e26c236e7dd59938054
Bach <waridh@ualberta.ca>
2022-01-15 14:22:05 -0700

    Finished print out function

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ def the_output_csv(filename, theData):
     The function that will output the data collected as a .csv file type.
     """
     address = "Outputs\\%s.csv"%(filename);
-    headerBoi = ["Price", "Link", "Title"];
+    headerBoi = ["Price", "Title", "Description", "Link"];
     with open(address, 'w') as fout:
         writer = csv.writer(fout);
         writer.writerow(headerBoi); # Prints out the headers for the data.
-        writer.writerow(theData);
+        for i in theData:
+            writer.writerow(i); # Shoving out data.
 
 if __name__ == "__main__":
-    databoi = ["test", 69, "cool"]
+    databoi = [["test", 69, "cool"], [420, "1070ti", "url"]];
     the_output_csv("test", databoi);
