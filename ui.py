fedd7941e0b3e8ba8db3814ef8c42651bcf0d837
echeng-git <eccheng@ualberta.ca>
2022-01-15 21:50:41 -0700

    UI prototype that works

--- ui.py
+++ ui.py
@@ -1,8 +1,12 @@
-# Later on auto install required libraries
+# Later on auto install required libraries shown below
+# pip install PyQt5
+# pip install pyqt-tools
+# pip install qdarkgraystyle
 
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout
 import sys
+import qdarkgraystyle
 
 class mainWindow(QMainWindow):
     def __init__(self):
@@ -11,31 +15,32 @@ class mainWindow(QMainWindow):
         # screen = QtWidgets.QDesktopWidget().screenGeometry(-1) # use screen.width() and screen.height()
         # (0,0) is the top left corner of screen
         # .setGeometry(xpos, ypos, width, height)
-        self.setGeometry(300, 300, 1500, 1000)
+        # self.setGeometry(300, 300, 1500, 1000)
         self.setWindowTitle("Super basic UI test with widgets")
+        self.setGeometry(1000,1000, 400, 400)
         self.initUI()
+
 
     def initUI(self):
         # Widgets that we want in the window (labels, etc)
         self.label = QtWidgets.QLabel(self)
         self.label.setText('Type something into the box!')
         self.label.adjustSize()
-        self.label.move(50,50)
+        self.label.move(25,25)
         
         # Input line
         self.input = QtWidgets.QLineEdit(self)
         self.input.setDragEnabled(True) # Can drag text within text box, as well as copy and paste
         self.input.setPlaceholderText("What are you looking for?")
         self.input.resize(250,30)
-        self.input.move(200,200)
+        self.input.move(25,100)
         self.input.returnPressed.connect(self.update_text) # Hit enter
-        # Call main or something on this line
 
         # Reset text button
         self.button = QtWidgets.QPushButton(self)
         self.button.setText('Press me to reset the label!')
         self.button.adjustSize()
-        self.button.move(300,50)
+        self.button.move(200,25)
         self.button.clicked.connect(self.reset_text) # Reset
 
     
@@ -56,7 +61,7 @@ class mainWindow(QMainWindow):
 
 def window(): # Defines application called window
     app = QApplication(sys.argv)
-    app.setStyle('Fusion')
+    app.setStyleSheet(qdarkgraystyle.load_stylesheet())
     win = mainWindow()
     win.show() # Shows the window
     sys.exit(app.exec_()) # Closes application when you hit "X"
