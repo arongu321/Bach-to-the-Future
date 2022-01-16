7e2ea52a6eef5a4435ea527a1cfa5466ee800012
Nikolai <notnikolai1@gmail.com>
2022-01-15 19:29:15 -0700

    moved get_page to util

--- ebay.py
+++ ebay.py
@@ -1,26 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import time
-
-
-def get_page(url):
-    try:
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-
-        if len(list(soup.children)) < 3:
-            print('Invalid response, wait 5s')
-            time.sleep(5)
-            page = requests.get(url)  # try again if the response was incorrect
-            soup = BeautifulSoup(page.content, 'html.parser')
-
-            if len(list(soup.children)) < 3:
-                print('Still invalid, giving up')
-                return None
-        return soup
-    except:
-        return None
-
+from util import get_page
 
 def get_ebay_page_info(url):
     page = requests.get(url)
