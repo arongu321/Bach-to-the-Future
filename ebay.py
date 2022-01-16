6ae5657ae84c5c5e68c8a9920d43a15257d1a6bd
Albert Li <41648834+AlbertLeeee@users.noreply.github.com>
2022-01-15 20:57:17 -0700

    Update ebay.py
    lol

--- ebay.py
+++ ebay.py
@@ -2,26 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 from storageBoi import StorageBoi
+from util import get_page
 
-
-
-def get_page(url):
-    try:
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-
-        if len(list(soup.children)) < 3:
-            print('Invalid response, wait 5s')
-            time.sleep(5)
-            page = requests.get(url)  # try again if the response was incorrect
-            soup = BeautifulSoup(page.content, 'html.parser')
-
-            if len(list(soup.children)) < 3:
-                print('Still invalid, giving up')
-                return None
-        return soup
-    except:
-        return None
 
 
 def get_ebay_page_info(url):
