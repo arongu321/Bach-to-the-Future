f246ca1eec585f7f6d7890829764209642173afe
Bach <waridh@ualberta.ca>
2022-01-15 17:52:02 -0700

    un testing

--- main.py
+++ main.py
@@ -59,16 +59,16 @@ def resort_prep(datA):
 if __name__ == "__main__":
     outname = input_and_name();
     search_terM = searchingp();
-    obj1 = StorageBoi(pricE = [420, 'CAD', 'BUYING'], urL = 'website',
-    titlE = 'Thing', descriptioN= 'fdslakfjdsklfjsdklfjsdklfjsdklj',
-    categorY = 'Yormum')
-    obj2 = StorageBoi(pricE = [69, 'CAD', 'BUYING'], urL = 'website',
-    titlE = 'Thing2', descriptioN= 'fdsdasfggfjsdklfjsdklj',
-    categorY = 'Yormum')
-    obj3 = StorageBoi()
-    # kijiji_list = kijiji_main(search_terM);
-    # print(kijiji_list);
-    test_list = [obj1, obj2, obj3]
+    # obj1 = StorageBoi(pricE = [420, 'CAD', 'BUYING'], urL = 'website',
+    # titlE = 'Thing', descriptioN= 'fdslakfjdsklfjsdklfjsdklfjsdklj',
+    # categorY = 'Yormum')
+    # obj2 = StorageBoi(pricE = [69, 'CAD', 'BUYING'], urL = 'website',
+    # titlE = 'Thing2', descriptioN= 'fdsdasfggfjsdklfjsdklj',
+    # categorY = 'Yormum')
+    # obj3 = StorageBoi()
+    kijiji_list = kijiji_main(search_terM);
+    print(kijiji_list);
+    # test_list = [obj1, obj2, obj3]
     databoi = [];
     databoi.extend(test_list);
     datagirl = resort_prep(databoi);
